dcc84cfd6bb9f80832babf0b2b33d2148eed39ee
Kat <187942808+northernpaws@users.noreply.github.com>
2025-12-22 18:39:29 -0700

    merge latest `dasp_frame` commit into audio module

--- crates/catalina-engine/src/audio/mod.rs
+++ crates/catalina-engine/src/audio/mod.rs
@@ -2,14 +2,13 @@ pub mod oscillator;
 
 // Re-export `dasp` so the version used by the engine
 // can be transparently imported by other crates.
-pub use dasp::{
-    Frame,
-    frame::{self, Mono, Stereo},
-    slice,
-};
+pub use dasp::slice;
 
 pub mod sample;
 pub use sample::{FromSample, Sample};
+
+pub mod frame;
+pub use frame::{Frame, Mono, Stereo};
 
 pub trait AudioSource {
     type Frame: Frame;
